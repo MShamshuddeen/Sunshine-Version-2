53476132326a1d2f9c8aea4fa6a70dba5a7e8b9b
mshamshuddeen <shamshuddeen@hotmail.com>
2017-01-02 13:51:36 +0530

    adding list_item_forecast

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -8,6 +8,14 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static android.R.attr.data;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -57,6 +65,30 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            String[] forecastArray = {
+                    "Mon 6/23 - Sunny - 31/17",
+                    "Tue 6/24 - Foggy - 21/8",
+                    "Wed 6/25 - Cloudy - 22/17",
+                    "Thurs 6/26 - Rainy - 18/11",
+                    "Fri 6/27 - Foggy - 21/10",
+                    "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                    "Sun 6/29 - Sunny - 20/7"
+            };
+            List<String> weeekforecast  = new ArrayList<String>(
+                    Arrays.asList(forecastArray));
+
+            ArrayAdapter<String> mforecastAdapter = new ArrayAdapter<String>(
+                    getActivity(),
+                    R.layout.list_item_forecast,
+                    R.id.list_item_forecast_textview,
+                    weeekforecast);
+
+
+            ListView listView = (ListView) rootView.findViewById(R.id.lisview_forecast);
+            listView.setAdapter(mforecastAdapter);
+
+
+
             return rootView;
         }
     }
